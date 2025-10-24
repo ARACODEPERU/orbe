35dd9b54a7ad42d5ad08628750d7cde99b8264a6
José Carlos <grande_josh@outlook.com>
2025-10-24 16:50:24 -0500

    d
    d

--- resources/views/components/brand.blade.php
+++ resources/views/components/brand.blade.php
@@ -15,34 +15,6 @@
                             </div>
                         </div>
                     @endforeach
-
-
-
-                    <div class="swiper-slide">
-                        <div class="brand__item bor radius-10 text-center p-4">
-                            <img src="themes/webpage/assets/images/brand/brand2.png" alt="icon">
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="brand__item bor radius-10 text-center p-4">
-                            <img src="themes/webpage/assets/images/brand/brand3.png" alt="icon">
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="brand__item bor radius-10 text-center p-4">
-                            <img src="themes/webpage/assets/images/brand/brand4.png" alt="icon">
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="brand__item bor radius-10 text-center p-4">
-                            <img src="themes/webpage/assets/images/brand/brand5.png" alt="icon">
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="brand__item bor radius-10 text-center p-4">
-                            <img src="themes/webpage/assets/images/brand/brand6.png" alt="icon">
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
