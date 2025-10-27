729a30120249e0e67a49e00eb9a740778efec19b
José Carlos <grande_josh@outlook.com>
2025-10-27 16:29:32 -0500

    Revert "Update web.php"
    This reverts commit f8e32daf5064d11c258c7301f5e944c08da6c39a.

--- routes/web.php
+++ routes/web.php
@@ -225,7 +225,6 @@ Route::middleware('auth')->group(function () {
     Route::post('meta/whatsapp/message/send', [MetaController::class, 'sendMessageWhatsapp'])->name('meta_whatsapp_message_send');
 });
 
-
 //CERTIFICADOS AUTOMATIZACIÓN Y PRUEBAS
 Route::get('/test-image/{student_id}/{certificate_id}', [WebController::class, 'testimage'])->name('test-image');
 
